682d8ce7acbac14ba3b01eb292b5300ed21d4ae7
subeeen <rose9165676@naver.com>
2026-03-29 13:52:34 +0900

    [feat] 환경별 설정 분리 및 예외 처리 개선

--- src/main/java/com/neuromove/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neuromove/backend/global/exception/GlobalExceptionHandler.java
@@ -5,7 +5,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -18,6 +20,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
+        log.error("Unhandled server exception occurred", e);
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ApiResponse.error("서버 내부 오류가 발생했습니다."));
