37417c863a8e441a1e4a3bd410473e676ebc5029
subeeen <rose9165676@naver.com>
2026-04-06 12:45:46 +0900

    feat: UserRepository 추가

---
+++ src/main/java/com/neuromove/backend/domain/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.neuromove.backend.domain.user.repository;
+
+import com.neuromove.backend.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
+}
