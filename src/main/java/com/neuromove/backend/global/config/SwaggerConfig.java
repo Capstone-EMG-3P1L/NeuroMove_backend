6d25f3f6fb786b242df021fe590ecc2ce2868261
subeeen <rose9165676@naver.com>
2026-04-06 15:12:25 +0900

    feat: JWT 검증 필터 및 보호 엔드포인트 적용

--- src/main/java/com/neuromove/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/neuromove/backend/global/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.neuromove.backend.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +12,21 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String jwtSchemeName = "BearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("NeuroMove Backend API")
                         .description("NeuroMove 백엔드 API 문서입니다.")
-                        .version("v1.0.0"));
+                        .version("v1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes(jwtSchemeName,
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
